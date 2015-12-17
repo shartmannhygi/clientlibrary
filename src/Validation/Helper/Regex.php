2f02bb0b02ecb7cc0c151a30af67e9af2d8e06af
Michael <michael@edmonndscommerce>
2015-12-17 11:50:55 +0000

    added space support

--- src/Validation/Helper/Regex.php
+++ src/Validation/Helper/Regex.php
@@ -5,9 +5,9 @@ namespace Upg\Library\Validation\Helper;
 
 class Regex
 {
-    const REGEX_FULL_ALPHANUMERIC = '/[\p{Cyrillic}\p{Latin}\p{Greek}0-9]*/';
-    const REGEX_FULL_ALPHA = '/[\p{Cyrillic}\p{Latin}\p{Greek}]*/';
+    const REGEX_FULL_ALPHANUMERIC = '/[\p{Cyrillic}\p{Latin}\p{Greek}\s0-9]*/';
+    const REGEX_FULL_ALPHA = '/[\p{Cyrillic}\p{Latin}\p{Greek}\s]*/';
 
-    const REGEX_PARTIAL_ALPHANUMERIC = '[\p{Cyrillic}\p{Latin}\p{Greek}0-9]';
-    const REGEX_PARTIAL_ALPHA = '[\p{Cyrillic}\p{Latin}\p{Greek}]';
+    const REGEX_PARTIAL_ALPHANUMERIC = '[\p{Cyrillic}\p{Latin}\p{Greek}\s0-9]';
+    const REGEX_PARTIAL_ALPHA = '[\p{Cyrillic}\p{Latin}\p{Greek}\s]';
 }
